3b4bc0a990ae99f1c4e25abf9d8e9a532ce07728
Sunny Singh <sunnysingh706050@gmail.com>
2024-09-16 08:44:33 +0530

    update next-config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@
 import {withContentlayer} from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode: true, swcMinify: true};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.devtool = "source-map"; // Enable source maps for better debugging
+    }
+    return config;
+  },
+};
 
 export default withContentlayer(nextConfig);
